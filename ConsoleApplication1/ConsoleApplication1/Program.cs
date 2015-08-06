cf4a357b0413edcbf46d02ca91887515dcbda7d8
Filly01 <filly.adem@live.com>
2015-08-06 10:00:31 -0700

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Alhamdulilah");
+            Console.WriteLine("Subhanellah");
         }
     }
 }
